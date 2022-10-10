7ebac8ea4cbb52ac88343de79d06ca65d96ca4a6
Taito Ohsumi <ohsumi-taito035@g.ecc.u-tokyo.ac.jp>
2022-10-10 18:28:29 +0900

    Add 42header and comments

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tohsumi <tohsumi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 18:28:16 by tohsumi           #+#    #+#             */
+/*   Updated: 2022/10/10 18:28:19 by tohsumi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
@@ -11,6 +23,16 @@
 
 typedef struct s_table	t_table;
 
+/*
+   Parameters
+   id         philosophers id.
+   thread     each philosopher should be a thread.
+   last_eat   time[ms] when philosopher last ate meal.
+   neats      number of time philosopher ate meal.
+   left       left-side fork.
+   right      right-side fork.
+   table      pointer to the table information.
+   */
 typedef struct s_philo
 {
 	int				id;
@@ -22,6 +44,20 @@ typedef struct s_philo
 	t_table			*table;
 }	t_philo;
 
+/*
+   Parameters
+   n_philos          number of philosophers.
+   time_d            time to die.
+   time_e            time to eat.
+   time_s            time to sleep.
+   n_time_must_eat   number of times each philosopher must eat.
+   is_dead           flag if the philosophers is dead or not.
+   start             time[ms] when start simulation.
+   philos            pointer to each philosopher.
+   forks             pointer to all the forks.
+   print             mutex in order to display timestamp in chronological order
+                     and not to display any log when one of philosophers dead.
+   */
 typedef struct s_table
 {
 	int				n_philos;
@@ -37,8 +73,8 @@ typedef struct s_table
 }	t_table;
 
 # define USEERR "[usage] ./philo number_of_philosophers \
-time_to_die time_to_eat time_to_sleep \
-[number_of_times_each_philosophers_must_eat]"
+	time_to_die time_to_eat time_to_sleep \
+	[number_of_times_each_philosophers_must_eat]"
 
 # define EMUTEX "pthread_mutex_init() failed"
 # define EMALLOC "malloc() failed"
@@ -54,22 +90,38 @@ enum e_actions
 	PDIED
 };
 
+// check cmdline arguments.
 int			check_args(int argc, char **argv);
 
+// initialize table.
+// This function contains reserving resources that must be released later.
 int			init_table(int argc, char **argv, t_table *table);
+int			launch_simulation(t_table *table);
 
+// release resources.
 int			destroy_table(t_table *table);
-long		ft_atoi(const char *nptr);
-int			ft_error(char *msg);
-long long	m_time(void);
-void		print_actions(t_philo *philo, int msg);
 
+/* philosophers' actions */
 void		philo_think(t_philo *philo);
 void		philo_sleep(t_philo *philo);
 void		philo_drop_forks(t_philo *philo);
 void		philo_eat(t_philo *philo);
 void		philo_take_forks(t_philo *philo);
+
+/* observer's actions */
 void		*die_check(void *arg);
-int			launch_simulation(t_table *table);
+
+/* utils */
+// similar to atoi() but return LONG_MIN in case of error.
+long		ft_atoi(const char *nptr);
+
+// stdout error message.
+int			ft_error(char *msg);
+
+// return timestamp[ms].
+long long	m_time(void);
+
+// stdout timestamp
+void		print_actions(t_philo *philo, int msg);
 
 #endif
